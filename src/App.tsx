b8abea37e16b06b5436134957b82b74fc58a74c8
kimyziii <yeaji105@naver.com>
2023-09-21 14:54:47 +0900

    로그인 및 회원가입 기능 추가 && 인증 관련해 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import { useState, useEffect } from "react";
+import { app } from "./firebaseApp";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import Router from "components/Router";
+import Loader from "components/Loader";
 
 function App() {
-  return <Router />;
+  const auth = getAuth(app);
+
+  // auth 체크 이전에 spinner를 띄워주는 용도
+  const [init, setInit] = useState<boolean>(false);
+  // auth에 currentUser가 있으면 true
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  );
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
+
+  return (
+    <>
+      <ToastContainer />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
+    </>
+  );
 }
 
 export default App;
--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "pages/home";
 import LoginPage from "pages/login";
@@ -7,20 +9,33 @@ import PostEdit from "pages/posts/edit";
 import PostNew from "pages/posts/new";
 import ProfilePage from "pages/profile";
 import SignupPage from "pages/signup";
+import Loader from "./Loader";
 
-export default function Router() {
+interface RouterProps {
+  isAuthenticated: boolean;
+}
+
+export default function Router({ isAuthenticated }: RouterProps) {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<PostList />} />
-        <Route path="/posts/:id" element={<PostDetail />} />
-        <Route path="/posts/new" element={<PostNew />} />
-        <Route path="/posts/edit/:id" element={<PostEdit />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="*" element={<Navigate replace to="/" />} />
+        {isAuthenticated ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/posts" element={<PostList />} />
+            <Route path="/posts/:id" element={<PostDetail />} />
+            <Route path="/posts/new" element={<PostNew />} />
+            <Route path="/posts/edit/:id" element={<PostEdit />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="*" element={<LoginPage />} />
+          </>
+        )}
       </Routes>
     </>
   );
