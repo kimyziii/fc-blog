8168ee9655f39d49dd795a5403c071a68cf609f5
kimyziii <yeaji105@naver.com>
2023-09-21 16:13:50 +0900

    게시글 상세 가져오기 (getDoc)

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,26 +1,58 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { useState } from "react";
+import { PostProps } from "./PostList";
+import { useEffect } from "react";
+import { db } from "firebaseApp";
+import Loader from "./Loader";
 
 export default function PostDetail() {
+  const [post, setPost] = useState<PostProps | null>(null);
+  const params = useParams();
+
+  const getPost = async (id: string) => {
+    if (id) {
+      const docRef = doc(db, "posts", id);
+      const docSnap = await getDoc(docRef);
+
+      setPost({ id: docSnap?.id, ...(docSnap.data() as PostProps) });
+    }
+  };
+
+  const handleDelete = () => {
+    console.log(`delete!`);
+  };
+
+  useEffect(() => {
+    if (params?.id) getPost(params?.id);
+  }, [params?.id]);
+
   return (
     <>
       <div className="post__detail">
-        <div className="post__box">
-          <div className="post__title">오늘의 일기다</div>
-          <div className="post__profile-box">
-            <div className="post__profile" />
-            <div className="post__author-name">패스트캠퍼스</div>
-            <div className="post__date">2023.09.19 화요일</div>
-          </div>
-          <div className="post__utils-box">
-            <div className="post__delete">삭제</div>
-            <div className="post__edit">
-              <Link to={`/posts/edit/1`}>수정</Link>
+        {post ? (
+          <div className="post__box">
+            <div className="post__title">{post?.title}</div>
+            <div className="post__profile-box">
+              <div className="post__profile" />
+              <div className="post__author-name">{post?.email}</div>
+              <div className="post__date">{post?.createdAt}</div>
+            </div>
+            <div className="post__utils-box">
+              <div className="post__delete" onClick={handleDelete}>
+                삭제
+              </div>
+              <div className="post__edit">
+                <Link to={`/posts/edit/1`}>수정</Link>
+              </div>
+            </div>
+            <div className="post__text post__text--pre-wrap">
+              {post?.content}
             </div>
           </div>
-          <div className="post__text">
-            너무 졸리고 집인데 집에 가고 싶고 뭐 그런{" "}
-          </div>
-        </div>
+        ) : (
+          <Loader />
+        )}
       </div>
     </>
   );
